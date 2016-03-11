ac69255ef10b40315d5fe32c7bec6146a4d08ed0
rrg92 <rodrigor.gomes@hotmail.com>
2016-03-10 23:53:34 -0300

    Response for Day 1
    Solution for day 1 created. No perfomance was take in consideration. The
    scripts runs about 170ms.

--- Day1.ps1
+++ Day1.ps1
@@ -1,1 +1,30 @@
-#This is solution for day 1
+#This is solution for day 1.
+#Created by Rodrigo Ribeiro Gomes
+
+#Stops on error!
+$ErrorActionPreference="Stop";
+
+#Puts current location on the location stack, before we change current location
+push-location
+
+#Get the directory of this script and change to it!
+set-location (Split-Path -Parent $MyInvocation.MyCommand.Definition )
+
+#Read file!
+$Input = Get-Content Day1INput.txt
+
+$CurrentFloor = 0;
+
+$Input.ToCharArray() | %{
+
+	#Assume that exists just
+	if($_ -eq "("){
+		$CurrentFloor++;
+	} 
+	elseif ($_ -eq ")") {
+		$CurrentFloor--;
+	}
+
+}
+
+write-host "Floor is: $CurrentFloor"
